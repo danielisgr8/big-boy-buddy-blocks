a0d00224cf741067f3d86259dd5cc22dc9cb7c7a
Isaac Spanier <ispanier@iastate.edu>
2019-11-02 12:14:35 -0500

    finished the login page v1

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import {Input, Button} from 'antd';
+import {Input, Button, Typography} from 'antd';
 import './login.css';
+const {Title} = Typography;
 
 
 export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {myName: ''}
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+        handleChange(event){
+            this.setState({myName: event.target.value});
+            console.log(this.state.myName);
+        }
+
+        handleSubmit(){
+            console.log('A name was submitted: ' + this.state.myName);
+        }
+
     render(){
         return(
-                <>
-                    <b>Big Boy Buddy Blocks</b>
-                    <Input className = "username" placeholder = "Enter Username" size = "default" />
-                    <Button className = "play" type = "primary">Play</Button>
-                </>
-        )
-    }
+            
+                     <span className = "login-flex">
+                     <Title level = {2} className = "bbbb-title">Big Boy Buddy Blocks</Title>
+                     <form onSubmit = {this.handleSubmit} className = "login-form">
+                         <Input onChange = {this.handleChange} values= {this.state.myName} className = "username" placeholder = "Enter Username" size = "default" />
+                         <br/>
+                        <Button onClick = {this.handleSubmit} type = "primary" className = "play" value = "Play">Play</Button>
+                     </form>
+                     </span>
+                )}
 }
+
