fca9871b8f99570d9c39940e0d9f5f9bf761abc4
Isaac Spanier <ispanier@iastate.edu>
2019-11-02 13:19:39 -0500

    login page links to the game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import React, {useState} from 'react';
 import './App.css';
 import Login from './Login';
+import Board from './Board';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState("false");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return(
     <div>
       {isLoggedIn ? (
-        <Login/>
-      ) : (
         <Board drawing="3"></Board>
+      ) : (
+        <Login onSubmit={(myName)=>{setIsLoggedIn(true); console.log("in app: " + myName)}}/>
       )}
     </div> 
   );
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,39 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Input, Button, Typography} from 'antd';
 import './login.css';
 const {Title} = Typography;
 
+const Login = ({onSubmit}) => {
+    const [myName, setMyName] = useState("");
 
-export default class Login extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {myName: ''}
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitting name: " + myName);
+        onSubmit(myName);
     }
-        handleChange(event){
-            this.setState({myName: event.target.value});
-            console.log(this.state.myName);
-        }
 
-        handleSubmit(event){
-            console.log('A name was submitted: ' + this.state.myName);
-            event.preventDefault();
-        }
-
-    render(){
-        return(
-            
-                     <span className = "login-flex">
-                     <Title level = {2} className = "bbbb-title">Big Boy Buddy Blocks</Title>
-                     <form onSubmit = {this.handleSubmit} className = "login-form">
-                         <Input onChange = {this.handleChange} values= {this.state.myName} className = "username" placeholder = "Enter Username" size = "default" />
-                         <br/>
-                        <Button onClick = {this.handleSubmit} type = "primary" className = "play" value = "Play">Play</Button>
-                     </form>
-                     </span>
-                )}
+    return(
+        <span className = "login-flex">
+        <Title level = {2} className = "bbbb-title">Big Boy Buddy Blocks</Title>
+        <form onSubmit = {handleSubmit} className = "login-form">
+            <Input onChange = {(e) => setMyName(e.target.value)} values= {myName} className = "username" placeholder = "Enter Username" size = "default" />
+            <br/>
+           <Button onClick = {handleSubmit} type = "primary" className = "play" value = "Play">Play</Button>
+        </form>
+        </span>
+   )
 }
 
+export default Login;
