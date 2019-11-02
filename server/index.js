d40b9b70045e4a6970644db9032e483b0116f249
danielisgr8 <schubertmdaniel@gmail.com>
2019-11-01 20:41:39 -0500

    Finished server set-up

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static("../client/public", {
+  extensions: ["html"]
+}));
 
-app.listen(port, () => console.log("Server listening on port ${port}!"));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
