36e3b421384bc00aae5174763a1b289a09768276
Jake <jrtener@iastate.edu>
2019-11-02 01:45:55 -0500

    Piece Drawings Done

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Drawing from './DrawObject';
+
+function App() {
+  return (
+    <Drawing drawing="5"></Drawing>
+  );
+  
+}
+export default App;
