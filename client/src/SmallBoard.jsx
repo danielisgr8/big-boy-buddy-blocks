e3d90da1c62b4e350d760e8644e00678b0034528
danielisgr8 <schubertmdaniel@gmail.com>
2019-11-04 00:36:40 -0600

    SmallBoard clean-up
    - Fixed whitespace
    - Removed unnecessary logging
    - canvas.focus is no longer called, as that is a no-op
    - Added a TODO to combine Board and SmallBoard in the future
    - Removed unused import

--- client/src/SmallBoard.jsx
+++ client/src/SmallBoard.jsx
@@ -1,53 +1,49 @@
 import React,{ useRef, useEffect }from 'react';
-import { BoardState, blockTypes } from './board-state/board-state';
 import Draw from "./draw";
+import { BoardState } from './board-state/board-state';
 import Mediator from './board-state/mediator';
 
-export const SmallBoard = ({color}) => {
-    console.log("Small Board->", color );
-    const canvasRef = useRef();
-    const boardRef = useRef();
-    const boardStateRef = useRef();
-    const myBlockRef = useRef();
+// TODO: make common Board component. Has displayOnly, as well as width and height (in number of cells) props
+export const SmallBoard = ({ color }) => {
+  const canvasRef = useRef();
+  const boardRef = useRef();
+  const boardStateRef = useRef();
+  const myBlockRef = useRef();
 
+  const render = () => {
+    const board = boardRef.current;
+    const boardState = boardStateRef.current;
+    requestAnimationFrame(() => {
+      board.drawState(boardState.state);
+      render();
+    })
+  }
 
-    
-    const render = () => {
-        const board = boardRef.current;
-        const boardState = boardStateRef.current;
-        requestAnimationFrame(() => {
-          board.drawState(boardState.state);
-          render();
-        })
-      }
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        canvas.focus();
-        const ctx = canvas.getContext("2d");
-    
-        const cellWidth = canvas.width / 4;
-        const cells = Math.round(canvas.width / cellWidth);
-        const board = new Draw(ctx, canvas.width, canvas.height, cellWidth)
-        boardRef.current = board;
-      
-        const boardState = new BoardState(cells, cells);
-        boardStateRef.current = boardState;
-        console.log("SmallBoard Color:", color);
+    const cellWidth = canvas.width / 4;
+    const cells = Math.round(canvas.width / cellWidth);
+    const board = new Draw(ctx, canvas.width, canvas.height, cellWidth)
+    boardRef.current = board;
+  
+    const boardState = new BoardState(cells, cells);
+    boardStateRef.current = boardState;
     const myBlock = boardState.addBlock(boardState.getRandomType(), color, "greg", false);
-        myBlockRef.current = myBlock;
-        
+    myBlockRef.current = myBlock;
+    
     Mediator.setPreviewCallback(() => {  
-          const blockType = myBlockRef.current.piece;
-          console.log(blockType);
-          boardState.clear();
+      const blockType = myBlockRef.current.piece;
+      boardState.clear();
       myBlockRef.current = boardState.addBlock(boardState.getRandomType(), color, "greg", false);
-          return blockType;
-        });
-        render();
-      }, []);
+      return blockType;
+    });
 
-      
-      return (
-          <canvas style = {{margin:"auto"}} ref={canvasRef} width={0.05 * window.innerWidth} height={0.05 * window.innerWidth}/>);
+    render();
+  }, []);
+
+  return (
+    <canvas style = {{margin:"auto"}} ref={canvasRef} width={0.05 * window.innerWidth} height={0.05 * window.innerWidth}/>
+  );
 };
